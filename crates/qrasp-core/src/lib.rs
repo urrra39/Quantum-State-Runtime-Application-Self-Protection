2f02a6070286019114c29daf1dba202cb285ad8a
VIP777 <urrra39@gmail.com>
2026-06-03 07:35:07 +0000

    Add new file

---
+++ crates/qrasp-core/src/lib.rs
@@ -0,0 +1,9 @@
+//! qrasp-core
+//!
+//! Sof matematik va kvant holatlarini kuzatish yadrosi.
+
+pub mod observer;
+// Keyingi bosqichlarda qo'shiladigan modullar uchun joy:
+// pub mod metrics;
+// pub mod anomaly;
+// pub mod defense;
